754244088e273cb9c9c6110715c5831ba6329279
Ryan Hazama <ryanhazama@Ryans-Laptop.local>
2022-01-28 18:54:29 -0800

    connect to the MySQL database

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const mysql = require('mysql2');
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -5,6 +6,19 @@ const app = express();
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // Your MySQL username,
+      user: 'rhazama01@gmail.com',
+      // Your MySQL password
+      password: '',
+      database: 'election'
+    },
+    console.log('Connected to the election database.')
+  );  
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
