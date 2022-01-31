0ad777f3179401591ff864b033d98514b50861a0
Ryan Hazama <ryanhazama@Ryans-Laptop.local>
2022-01-31 15:51:28 -0800

    Added to partyRoutes.js and other js files in root folder

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-const db = require('./db/connection');
 const express = require('express');
+const db = require('./db/connection');
 const apiRoutes = require('./routes/apiRoutes');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-/// Express middleware
+// Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
