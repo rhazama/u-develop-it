61f983efe28837aa263b92987456b1c7c677def5
Ryan Hazama <ryanhazama@Ryans-Laptop.local>
2022-02-01 15:12:42 -0800

    added voter routes

--- root/routes/apiRoutes/index.js
+++ root/routes/apiRoutes/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-router.use(require('./candidateRoutes'));
+router.use(require('./condidateRoutes'));
 router.use(require('./partyRoutes'));
 router.use(require('./voterRoutes'));
 
